fd354ff6e036a8415c06682ed4ea4a84a16babe5
Alan Cassar <a.cassar@marcura.com>
2020-08-04 15:36:13 +0400

    adding sample for working with XML

--- dis-client/sample-java-client/src/main/java/com/marcura/dis/SampleClient.java
+++ dis-client/sample-java-client/src/main/java/com/marcura/dis/SampleClient.java
@@ -1,4 +1,15 @@
 package com.marcura.dis;
+
+import java.io.ByteArrayOutputStream;
+import java.io.StringReader;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.namespace.QName;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -8,28 +19,77 @@ import com.dadesk.dis.schema.GetPortcallStatusInfoRequest;
 import com.dadesk.dis.schema.PortcallStatusInfoResponseType;
 
 public class SampleClient {
-	
-	public static void main(String[] args){
-		
-		//Start Spring
-		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
-		
-		//Get the CXF client
-		DaDeskDataExchange disClient = (DaDeskDataExchange) context.getBean("disClient");
-		
-		//Create new portcall status request
+
+	/**
+	 * Creates and sends a request based on the assumption that developer will be
+	 * working fully in Java and constructing all the objects in Java. All the SOAP,
+	 * XML and WS-Security will be handled by the framework and the developer will
+	 * not have to deal with any of that.
+	 * 
+	 * @param disClient
+	 */
+	public static void javaSample(DaDeskDataExchange disClient) {
+		// Create new portcall status request
 		GetPortcallStatusInfoRequest portcallStatusRequest = new GetPortcallStatusInfoRequest();
 		portcallStatusRequest.setInterfaceUniqueReference("PTO-170001");
 		portcallStatusRequest.setPrincipalId("50001");
-		
-		//Call portcall status info
+
+		// Call portcall status info
 		PortcallStatusInfoResponseType portcallStatusResponse = disClient.getPortcallStatusInfo(portcallStatusRequest);
-		
-		//Print the porcall reference number from the response
+
+		// Print the portcall reference number from the response
 		System.out.println("Reference number: " + portcallStatusResponse.getPortCallRef());
-		
-		//Close Spring
-		((ConfigurableApplicationContext)context).close();
+		System.out.println("Vessel: " + portcallStatusResponse.getVessel().getName());
+	}
+
+	/**
+	 * Creates and sends a request based on the assumption that the developer will
+	 * be having the request already in XML and simply requires the framework to
+	 * deal with the additional information for SOAP and WS-Security.
+	 * 
+	 * @param disClient
+	 * @throws JAXBException
+	 */
+	public static void xmlSample(DaDeskDataExchange disClient) throws JAXBException {
+		// initialise the JAXB marshaller and unmarshaller
+		JAXBContext jaxbContext = JAXBContext.newInstance("com.dadesk.dis.schema");
+		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+		Marshaller marshaller = jaxbContext.createMarshaller();
+
+		String xmlRequest = "<sch:getPortcallStatusInfoRequest xmlns:sch=\"http://www.dadesk.com/dis/schema\">\r\n"
+				+ "	<sch:principalId>50001</sch:principalId>\r\n"
+				+ "	<sch:interfaceUniqueReference>PTO-170001</sch:interfaceUniqueReference>\r\n"
+				+ "</sch:getPortcallStatusInfoRequest>";
+
+		// Read request from XML file and convert to Java object
+		GetPortcallStatusInfoRequest portcallStatusRequest = (GetPortcallStatusInfoRequest) unmarshaller
+				.unmarshal(new StringReader(xmlRequest));
+
+		// Call portcall status info
+		PortcallStatusInfoResponseType portcallStatusResponse = disClient.getPortcallStatusInfo(portcallStatusRequest);
+
+		// Convert the response into XML
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		marshaller.marshal(new JAXBElement<PortcallStatusInfoResponseType>(
+				new QName("http://www.dadesk.com/dis/schema", "getPortcallStatusInfoResponse"),
+				PortcallStatusInfoResponseType.class, portcallStatusResponse), baos);
+		System.out.println(baos.toString());
+
+	}
+
+	public static void main(String[] args) throws Exception {
+
+		// Start Spring
+		ApplicationContext context = new ClassPathXmlApplicationContext("spring-context.xml");
+
+		// Get the CXF client
+		DaDeskDataExchange disClient = (DaDeskDataExchange) context.getBean("disClient");
+
+		javaSample(disClient);
+		xmlSample(disClient);
+
+		// Close Spring
+		((ConfigurableApplicationContext) context).close();
 	}
 
 }
